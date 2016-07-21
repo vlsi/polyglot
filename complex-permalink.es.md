a9a854baa536badcf32f41f9dcee3f163040c014
Sam Volin <sam.volin@usmac2213.schq.secious.com>
2016-07-21 14:02:42 -0600

    Thu Jul 21 14:02:41 MDT 2016

--- complex-permalink.es.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: A really long permalink
-permalink: a-really-long/permalink/
-lang: es
----
-
-Este enlace permanente es muy largo y complejo
--- publi.sh
+++
@@ -1,9 +0,0 @@
-# publi.sh
-# change the branch names appropriately
-git checkout site
-rm -rf _site/
-jekyll build
-git add --all
-git commit -m "`date`"
-git push origin site
-git push origin `git subtree split --prefix _site/ site`:gh-pages --force
